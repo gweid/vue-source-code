90efffd0f4f38dbb14908eb81369ab2e16237b3c
官维 <guanwei@triumen.com>
2020-06-05 17:09:10 +0800

    vuex

--- vuex/src/mixin.js
+++ vuex/src/mixin.js
@@ -2,15 +2,16 @@ export default function (Vue) {
   const version = Number(Vue.version.split('.')[0])
 
   if (version >= 2) {
-    Vue.mixin({ beforeCreate: vuexInit })
+    Vue.mixin({
+      beforeCreate: vuexInit
+    })
   } else {
     // override init and inject vuex init procedure
     // for 1.x backwards compatibility.
     const _init = Vue.prototype._init
     Vue.prototype._init = function (options = {}) {
-      options.init = options.init
-        ? [vuexInit].concat(options.init)
-        : vuexInit
+      options.init = options.init ? [vuexInit].concat(options.init) :
+        vuexInit
       _init.call(this, options)
     }
   }
@@ -18,14 +19,15 @@ export default function (Vue) {
   /**
    * Vuex init hook, injected into each instances init hooks list.
    */
-
-  function vuexInit () {
+  // 最终每个 Vue 的实例对象，都有一个 $store 属性。且是同一个 Store 实例
+  function vuexInit() {
     const options = this.$options
     // store injection
+    // store 注入到每一个 Vue 实例中
     if (options.store) {
-      this.$store = typeof options.store === 'function'
-        ? options.store()
-        : options.store
+      this.$store = typeof options.store === 'function' ?
+        options.store() :
+        options.store
     } else if (options.parent && options.parent.$store) {
       this.$store = options.parent.$store
     }
