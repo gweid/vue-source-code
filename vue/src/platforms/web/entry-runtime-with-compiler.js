bfd355f5dc93408d21a9c798926e06a0ec6f86fd
gweid <guanwei006@gmail.com>
2020-08-04 01:20:32 +0800

    修改new Vue中的编译流程
    Vue.prototype. 执行编译

--- vue/src/platforms/web/entry-runtime-with-compiler.js
+++ vue/src/platforms/web/entry-runtime-with-compiler.js
@@ -69,7 +69,8 @@ Vue.prototype.$mount = function (
       if (process.env.NODE_ENV !== 'production' && config.performance && mark) {
         mark('compile')
       }
-
+    
+      // compileToFunctions 执行编译的函数
       const { render, staticRenderFns } = compileToFunctions(template, {
         outputSourceRange: process.env.NODE_ENV !== 'production',
         shouldDecodeNewlines,
